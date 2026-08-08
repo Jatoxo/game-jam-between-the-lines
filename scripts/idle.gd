d051755ade95352f1cb4ab04db311b3e7c33c464
Jatoxo <39669932+Jatoxo@users.noreply.github.com>
2026-08-09 00:34:28 +0200

    Add server and player disconnects

--- scripts/idle.gd
+++ scripts/idle.gd
@@ -1,3 +1,16 @@
-extends Control
+extends "res://scripts/ui_screen.gd"
 
+func _ready():
+	Lobby.player_disconnected.connect(player_disconnected)
+	
+
+func player_disconnected(id : int):
+	if id == 0:
+		print("SERVER DIED PANIC PANIC OH NOO")
+		go_back()
+
+# Leave the lobby on exit
+func on_go_back():
+	print("Disconnecting")
+	Lobby.remove_multiplayer_peer()
 	
