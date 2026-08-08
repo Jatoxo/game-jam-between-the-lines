693de89e07bd4a9402b6d3e1b5d2739ebc0e56eb
Jatoxo <39669932+Jatoxo@users.noreply.github.com>
2026-08-09 00:24:15 +0200

    Fix Player Creation

--- scripts/player_creation.gd
+++ scripts/player_creation.gd
@@ -1,13 +1,11 @@
 extends Control #PlayerCreation
-@export var avatar_size: int = 96
+@export var avatar_size: int = 150
 @export var avatar_group: ButtonGroup
 @export var avatar_textures: Array[Texture2D]  # index-matched to buttons
 var username : String
 var role : String
-var id = 0#multiplayer.multiplayer_peer
 
-func _ready() -> void:
-	_build_buttons(avatar_size)
+
 func get_selected_avatar() -> int:
 	var pressed_button = avatar_group.get_pressed_button()
 	if pressed_button == null:
@@ -15,15 +13,6 @@ func get_selected_avatar() -> int:
 	var index = pressed_button.get_index()+1
 	return index
 
-func _build_buttons(size:int):
-	for child in $VBoxContainer/GridContainer.get_children():
-		child.custom_minimum_size = Vector2(size, size)  
-		child.ignore_texture_size = true 
-		child.size_flags_vertical = Control.SIZE_EXPAND_FILL 
-		child.size_flags_horizontal = Control.SIZE_EXPAND_FILL 
-		child.stretch_mode = TextureButton.STRETCH_KEEP_ASPECT_CENTERED
-		child.ignore_texture_size = true
-	
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
 	username = new_text
@@ -32,7 +21,7 @@ func _on_line_edit_text_submitted(new_text: String) -> void:
 func _on_submit_pressed() -> void:
 	username = $VBoxContainer/UsernameInput.text
 	
-	PlayerData.set_player.rpc_id(1,id,username,get_selected_avatar())
+	PlayerData.set_player.rpc_id(1,username,get_selected_avatar())
 	if not username == null:
 		get_tree().change_scene_to_file("res://scenes/idle.tscn")
 		pass
