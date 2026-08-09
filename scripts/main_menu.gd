168af01e46a8a92cac9db18064a9846a5cf9fe9f
ha_sophie <hannah.meyer25@web.de>
2026-08-09 15:10:33 +0200

    some stuff

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -17,8 +17,3 @@ func _on_join_game_pressed() -> void:
 
 func _on_create_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/lobby/host_game.tscn")
-
-	
-func _on_mock_game_pressed() -> void:
-	MockServer.start_game()
-	get_tree().change_scene_to_file("res://scenes/role_assigment.tscn")
